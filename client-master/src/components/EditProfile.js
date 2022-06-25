f14b3f4e11bda14350efcad63d2810a7118ee132
antdawg925 <98189872+antdawg925@users.noreply.github.com>
2022-06-25 02:53:14 -0700

    Add files via upload

--- client-master/src/components/EditProfile.js
+++ client-master/src/components/EditProfile.js
@@ -1,15 +1,16 @@
-import React, {useState} from "react";
+// \\\\\\<$A$>///////\\\\\\<$A$>///////  _________  IMPORTS  _____________ \\\\\\<$A$>///////\\\\\\<$A$>///////
+//                                   -----------------------------------------
+import React, { useState } from "react";
 import axios from "axios";
 import "../CSS/EditProfile.css";
+// ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+// ----------------------------------------------------------------------------------------------------------
+// ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 
 const EditProfile = (props) => {
-
-  let selectedFileHandler = (e) => {
-    console.log(e);
-  }
-
-   // Post image to API
-    const axiosImage = (e) => {
+  // Post image to API
+  //---------- NOT YET IMPLEMENTED -----
+  const axiosImage = (e) => {
     e.preventDefault();
     axios.post("https://api.imgbb.com/1/upload?expiration=600&key=d5ed21ef35be30a8aa6fb22713ce529f&image=" + props.image)
       .then((res) => {
@@ -21,42 +22,37 @@ const EditProfile = (props) => {
       .catch(err => console.log(err))
   }
 
-  // const funct = (e) => {
-  //   e.preventDefault()
-  //   console.log(props.image);
-  // }
-
   return (
     <div className="center1">
-      <p className="pic">PROFILE IMAGE </p>
+      <p className="text-slate-700 ml-14 my-8 border-2 border-blue-900 w-1/6 text-center rounded-full">PROFILE <br /> IMAGE </p>
       {/* convert to base 64 string */}
       {/* ----------------------------------------------------------------------------------------------------------- */}
+      <h1 style={{marginLeft:"3.45rem"}}>{props.image}</h1>
       <form onSubmit={axiosImage}>
-        <input type="file" onChange={(e) => props.setImage(e.target.value)} />
-        <button className="edit-btn" onClick={(e)=> axiosImage(e)}>Save changes</button>
+        <input type="file" className="mx-14 text-base block text-slate-700" onChange={(e) => props.setImage(e.target.value)} />
+        <button className="edit-btn mx-14 my-4" onClick={(e) => axiosImage(e)}>Save changes</button>
       </form>
       {/* ----------------------------------------------------------------------------------------------------------- */}
-      <form >
-       
-          <input type="text" className="input" placeholder="First Name"/> 
+      <form className="flex flex-col">
 
-          <input type="text" className="input" placeholder="Last Name"/>  
+        <input type="text" className="text-base mx-14 my-2 p-1" placeholder="First Name" />
 
-          <input type="text" className="input" placeholder="Bio:" />
+        <input type="text" className="text-base mx-14 my-2 p-1" placeholder="Last Name" />
 
-          <input type="text" className="input" placeholder="Location:" />
+        <input type="text" className="text-base mx-14 my-2 p-1" placeholder="Bio:" />
 
-        <button className="edit-btn">Submit</button>
+        <input type="text" className="text-base mx-14 my-2 p-1" placeholder="Location:" />
+
+        <button className="edit-btn w-1/6 mx-14 my-4 min-w-fit">Submit</button>
 
       </form>
-      <h1>-- {JSON.stringify(props.image)} --</h1>
-      <button className="edit-btn btm"
-        onClick={()=> {
+      <button className="edit-btn btm mx-14"
+        onClick={() => {
           props.setEditProfile(false)
           props.setSettings(true)
-        }}>Adjust Your Settings!</button><br/>
+        }}>Adjust Your Settings!</button><br />
       <button className="cancel"
-        onClick={()=> props.setEditProfile(false)}>Cancel</button>
+        onClick={() => props.setEditProfile(false)}>Cancel</button>
 
     </div>
   );
--- client-master/src/components/IconNav.js
+++ client-master/src/components/IconNav.js
@@ -1,36 +1,58 @@
-import React, { useState } from "react";
+// \\\\\\<$A$>///////\\\\\\<$A$>///////  _________  IMPORTS  _____________ \\\\\\<$A$>///////\\\\\\<$A$>///////
+//                                   -----------------------------------------
+import React from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import People from "@mui/icons-material/People";
 import "../CSS/HomePage.css";
+import axios from "axios";
 import { useHistory } from "react-router-dom";
-import Post from "../components/Post";
+// ----------------------------------------------------------------------------------------------------------
+// ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 
 const IconNav = (props) => {
+  //  history.push("/") is used to navigate to other routes
   const history = useHistory();
   const handleHome = () => {
     history.push("/home/feed");
   };
 
-
+  const logOut = () => {
+    axios.get("/logout")
+      .then(res => {
+        console.log("users logout axios has been triggered -- ", res);
+        localStorage.setItem("logged_in", "no")
+        history.push("/")
+      })
+      .catch(err => {
+        console.log("logout axios somehting went wrong -- ", err);
+      })
+  }
 
 
   return (
-    <div id="icons" className="min-w-fit">
+    <div id="icons" className="min-w-fit px-2">
       <HomeIcon sx={{ fontSize: 100 }} onClick={() => handleHome()} />
       <NotificationsIcon sx={{ fontSize: 100 }} />
       <ChatBubbleIcon sx={{ fontSize: 100 }} />
-      <People sx={{ fontSize: 100 }} onClick={() => (history.push("/find/users"))}/>
+      <People sx={{ fontSize: 100 }} onClick={() => (history.push("/find/users"))} />
 
       <button className="
         button-10 rounded 
-        font-bold text-smS
-        p-2 w-1/2 min-w-fit 
-        absolute left-6 bottom-5" 
+        font-bold text-base
+        p-2 w-2/3 min-w-fit 
+        absolute left-6 bottom-16"
         onClick={() => props.renderPost()}
       >
         Create Post
+      </button>
+      <button className="button-10 rounded 
+        font-bold text-base
+        p-2 w-2/3 
+        absolute left-6 bottom-5"
+        onClick={() => logOut()}>
+        LOG OUT
       </button>
       {/* {postForm ? <Post setPostForm={setPostForm} /> : ""} */}
     </div>
