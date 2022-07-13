8a1a9b44ab096c4fa0fff86409699a06ef2a334f
antdawg925 <98189872+antdawg925@users.noreply.github.com>
2022-07-12 21:57:48 -0700

    Update Users_controllers.py

--- server/flask_server/controllers/Users_controllers.py
+++ server/flask_server/controllers/Users_controllers.py
@@ -1,5 +1,3 @@
-# \\\\\\<$A$>///////\\\\\\<$A$>///////  _________  IMPORTS  _____________ \\\\\\<$A$>///////\\\\\\<$A$>///////
-#                                   -----------------------------------------
 from flask import Flask, render_template, request, redirect, session, flash
 from flask_server import app
 from flask_server.models.User_Model import User
@@ -8,9 +6,6 @@ import re
 
 bcrypt = Bcrypt(app)
 
-# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-#                                        GET EVERY USER
-# ______________________________________________________________________________________________________
 @app.route("/every/user")
 def get_every_user():
     every_user = User.get_every_user()
@@ -18,18 +13,14 @@ def get_every_user():
     for user in every_user:
         catch.update({user["id"]: user})
     return catch
-# ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 
 
-# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-#                                        GET LOGGED IN USER
-# ______________________________________________________________________________________________________
+#  GET LOGGED IN USER
 @app.route('/user/info')
 def get_user_info():
     user = User.get_user()
     print("User id in controller",user)
     id_obj = user[0]
     return id_obj
-# ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 
 
