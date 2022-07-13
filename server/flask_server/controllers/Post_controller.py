ce8582e597a9ac53cc5a16b384ae696fecab7c2d
antdawg925 <98189872+antdawg925@users.noreply.github.com>
2022-07-12 21:57:28 -0700

    Update Post_controller.py

--- server/flask_server/controllers/Post_controller.py
+++ server/flask_server/controllers/Post_controller.py
@@ -1,15 +1,9 @@
-# \\\\\\<$A$>///////\\\\\\<$A$>///////  _________  IMPORTS  _____________ \\\\\\<$A$>///////\\\\\\<$A$>///////
-#                                   -----------------------------------------
 from cmath import log
 from flask import Flask, render_template, request, redirect, session, flash
 from flask_server import app
 from flask_server.models.Post_Model import Post
 from flask_server.models.User_Model import User
 
-
-# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-#                                         POST USERS POST
-# ______________________________________________________________________________________________________
 @app.route("/post/post", methods=["POST"])
 def create_post():
     post_data=request.get_json()
@@ -19,12 +13,9 @@ def create_post():
     }
     Post.save(data)
     return ("Post has been posted!")
-# ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 
 
-# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-#                                         GET FOLLOWING  POST
-# ______________________________________________________________________________________________________
+# GET FOLLOWING  POST
 @app.route("/following/posts")
 def get_every_post():
     every_post = Post.get_following_posts()
@@ -32,12 +23,9 @@ def get_every_post():
     for post in every_post:
         catch.update({post["id"]: post})
     return catch
-# ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
 
 
-# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-#                                         GET USERS POST
-# ______________________________________________________________________________________________________
+#  GET USERS POST
 @app.route("/users/posts")
 def get_users_posts():
     user_posts = Post.get_users_posts( )
@@ -45,5 +33,4 @@ def get_users_posts():
     for post in user_posts:
         catch.update({post["id"]: post})
     return catch
-# ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
  
