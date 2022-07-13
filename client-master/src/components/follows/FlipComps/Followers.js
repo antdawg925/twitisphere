1fa5b4c6feb1d4a18affbf3a180c761dca96dd7c
antdawg925 <98189872+antdawg925@users.noreply.github.com>
2022-07-12 22:05:02 -0700

    Add files via upload

--- client-master/src/components/follows/FlipComps/Followers.js
+++ client-master/src/components/follows/FlipComps/Followers.js
@@ -1,9 +1,62 @@
 
-
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 const Followers = () => {
+
+    const [follower, setFollower] = useState([])
+
+    const getFollower = () => {
+        axios.get('/follows')
+            .then(res => {
+                console.log("Got the follower Obj from GET request", res.data);
+                setFollower(res.data['followers'])
+                // pullFollower(res.data["follower"])
+            })
+            .then(() => {
+                console.log("follower -- ", follower);
+            })
+            .catch(err => console.log(err))
+    }
+
+    // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    //                             HANDLE ALL INFORMATION RECEIVED FROM SQL QUERY AND 
+    //                                       PROCESS ALL USERS POSTS 
+    // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    let pullFollower = (dict) => {
+        console.log("Received obj", dict);
+        let postArr = [];
+
+        for (let i = 0; i < dict.length; i++) {
+            postArr.push(dict[i].user_name)
+        }
+
+        console.log("ARR OF follower - ", postArr)
+        console.log(follower, "follower state var!");
+        return follower
+    };
+    // ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+
+    useEffect(() => {
+        getFollower()
+    }, [])
     return (
-        <div>
-            <h1>Follower comp</h1>
+        <div className="rounded">
+            <h1 className="bg-blue-200 rounded-t-md h-10 mb-6 pt-2 shadow-xl shadow-blue-300 text-xl">Follower </h1>
+            {/* {JSON.stringify(follower)} */}
+            {
+                follower ? (
+                    follower.map((user, idx) => {
+                        console.log("user -- ", user);
+                        return (
+                            <div key={idx}>
+                                {/* {JSON.stringify(user)} */}
+                                <h5>{user.user_name}</h5>
+                            </div>
+                        )
+                    })
+
+                ) : <p>"Not working"</p>
+            }
         </div>
     )
 }
--- client-master/src/components/follows/FlipComps/Following.js
+++ client-master/src/components/follows/FlipComps/Following.js
@@ -1,53 +1,60 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
 const Following = () => {
 
-    const [following, setFollowing] = useState("")
+    const [following, setFollowing] = useState([])
 
     const getFollowing = () => {
         axios.get('/follows')
-            .then (res => {
-                console.log("??????????",res.data["following"]);
-                // setFollowing(res.data['following'])
-                pullFollowing(res.data["following"])
-                console.log("following -- ",following);
-                
+            .then(res => {
+                console.log("Got the following Obj from GET request", res.data["following"]);
+                setFollowing(res.data['following'])
+                // pullFollowing(res.data["following"])
             })
+            .then(() => {
+                console.log("following -- ", following);
+            })
+            .catch(err => console.log(err))
     }
- 
-  let pullFollowing = (dict) => {
-    console.log(dict,")))))))");
-    let postObj = {};
-    let postArr = [];
 
-    for (let i = 0; i < dict.length; i++) {
-      postArr.push(dict[i].user_name)
-    }
-    for (let i = 0; i < postObj.length; i++) {
-      postArr.push(postObj[i], "")
-    }
-    console.log("EERRERER",postArr)
-    setFollowing(postArr)
-    console.log(following, "_________");
-    return following
-  };
+    // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    //                             HANDLE ALL INFORMATION RECEIVED FROM SQL QUERY AND 
+    //                                       PROCESS ALL USERS POSTS 
+    // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    let pullFollowing = (dict) => {
+        console.log("Received obj", dict);
+        let postArr = [];
 
-  
+        for (let i = 0; i < dict.length; i++) {
+            postArr.push(dict[i].user_name)
+        }
+
+        console.log("ARR OF FOLLOWING - ", postArr)
+        console.log(following, "following state var!");
+        return following
+    };
+    // ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+
     useEffect(() => {
         getFollowing()
     }, [])
-    return ( 
+    return (
         <div>
-            <h1>Following comp</h1>
+            <h1 className="bg-blue-200 rounded-t-md h-10 mb-6 pt-2 shadow-xl shadow-blue-300 text-lg">Following</h1>
+            {/* {JSON.stringify(following)} */}
             {
                 following ? (
-                    following.map((following, idx) => {
-                        <div key={idx}>
-                            <h5>{following[idx]}</h5>
-                        </div>
+                    following.map((user, idx) => {
+                        console.log("user -- ", user);
+                        return (
+                            <div key={idx}>
+                                {/* {JSON.stringify(user)} */}
+                                <h5>{user.user_name}</h5>
+                            </div>
+                        )
                     })
 
-                ) : null
+                ) : <p>"Not working"</p>
             }
         </div>
     )
