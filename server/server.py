5520123dbe74c51c631fab7cb2dba34781389f02
antdawg925 <antearly925@gmail.com>
2022-08-23 18:41:54 -0700

    v1 deployment

--- server/server.py
+++ server/server.py
@@ -8,14 +8,4 @@ from flask_server.controllers import Follows_controller
 from flask_server import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
-import flask_server
-from flask_server.controllers import Users_controllers
-from flask_server.controllers import Post_controller
-from flask_server.controllers import Login_Reg_controller
-from flask_server.controllers import Points_controller
-from flask_server.controllers import Follows_controller
-from flask_server import app
-
-if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
